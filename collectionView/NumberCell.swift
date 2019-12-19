0dafdbc34a0207f1877a3c231d221375489521c6
priyans <priyans05@gmail.com>
2019-12-20 10:55:08 +1100

    CustomCell class created

---
+++ collectionView/NumberCell.swift
@@ -0,0 +1,16 @@
+//
+//  NumberCell.swift
+//  collectionView
+//
+//  Created by PRIYANS on 20/12/19.
+//  Copyright © 2019 PRIYANS. All rights reserved.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = String(describing: NumberCell.self)
+    
+    
+}
