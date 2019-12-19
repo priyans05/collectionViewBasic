4d894ac1842e9e85ce6a6b608da93c4c790c58ed
priyans <priyans05@gmail.com>
2019-12-20 10:40:24 +1100

    Layout Configuration done.

--- collectionView/ViewController.swift
+++ collectionView/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = configureLayout()
     }
-
+    
+    private func configureLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let collectionViewSection = NSCollectionLayoutSection(group: group)
+        
+        return UICollectionViewCompositionalLayout(section: collectionViewSection)
+    }
 
 }
 
